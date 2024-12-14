9607e9a46c27d27142a1880a4fe8daed559832ab
AbhinavSharma486 <abhinavparashar486@gmail.com>
2024-12-14 14:21:45 +0530

    MongoDb database connection completed here

---
+++ backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDb Connected : ${conn.connection.host}`);
+
+  } catch (error) {
+    console.log("MongoDb connection failed : ", error);
+
+  }
+};
+
+export default connectDB;
